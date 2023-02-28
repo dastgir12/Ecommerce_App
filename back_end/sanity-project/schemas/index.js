af179c48af6ebb828f9612930b258573bc519e08
dastgir12 <111153739+dastgir12@users.noreply.github.com>
2023-02-28 23:08:39 +0500

    init

--- back_end/sanity-project/schemas/index.js
+++ back_end/sanity-project/schemas/index.js
@@ -3,5 +3,4 @@ import user from './user';
 import comment from './comment';
 import postedBy from './postedBy';
 import save from './save';
-
 export const schemaTypes = [pin, user, postedBy, comment, save]
--- front_end/my-project/src/components/Pin.jsx
+++ front_end/my-project/src/components/Pin.jsx
@@ -1,16 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
+import { MdDownloadForOffline } from 'react-icons/md';
+import { AiTwotoneDelete } from 'react-icons/ai';
+import { BsFillArrowUpRightCircleFill } from 'react-icons/bs';
+
 import { client, urlFor } from '../client';
 
 const Pin = ({ pin: { postedBy, image, _id, destination } }) => {
+  const [postHovered, setPostHovered] = useState(false);
+  const [savingPost, setSavingPost] = useState(false);
+ const navigate = useNavigate()
+ 
   const imageUrl = image ? urlFor(image).width(250).url() : null;
-
   return (
-    <div>
-      {imageUrl ? (
+    <div className='m-2'>
+<div
+onMouseEnter={()=>setPostHovered(true)}
+onMouseLeave={()=>setPostHovered(false)}
+onClick={()=> navigate(`/pin-detail/${_id}`)}>
+
+
+</div>
+
+
+
+
+
+      {/* {imageUrl ? (
         <img src={imageUrl} alt="user-post" className='rounded-lg w-full' />
       ) : (
         <div>Image not found</div>
-      )}
+      )} */}
     </div>
   )
 }
