c6ad08cb58749422e3c639c39695f0ad96f550dc
dastgir12 <111153739+dastgir12@users.noreply.github.com>
2023-03-01 13:00:56 +0500

    init

--- front_end/my-project/src/components/CreatePin.jsx
+++ front_end/my-project/src/components/CreatePin.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { AiOutlineCloudUpload } from 'react-icons/ai';
+import { useNavigate } from 'react-router-dom';
+import { MdDelete } from 'react-icons/md';
+import { categories } from '../utils/data';
+import { client } from '../client';
+import Spinner from './Spinner';
 
-const CreatePin = () => {
+
+const CreatePin = ({user}) => {
   return (
     <div>
-      
+      Create Pin PAGE 
     </div>
   )
 }
---
+++ front_end/my-project/src/components/usefulLogics.js
@@ -0,0 +1,14 @@
+//here we define just great logics which we can apply future i  our code :
+
+// if we want to make a statement as bolean form but statement return integre type form then we have to change it into boolean
+// here 0 , 1 represent false and true respectively 
+
+
+//logic:
+
+//we use !! to convert integer into boolean .
+
+// wrap statement into bracket and then make it .length
+
+// const alreadySaved = !!(save?.filter((item)=>item.postedBy._id === user.sub)).length;
+//
