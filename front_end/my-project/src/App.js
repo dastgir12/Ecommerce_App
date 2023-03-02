77ad77d554ee38fa9ec39dcf40005df8f921dae5
dastgir12 <111153739+dastgir12@users.noreply.github.com>
2023-03-02 10:32:22 +0500

    init

--- front_end/my-project/src/App.js
+++ front_end/my-project/src/App.js
@@ -1,15 +1,24 @@
-import { Routes , Route , useNavigate } from 'react-router';
-import Login from './components/Login';
+import React, { useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+
+import { Login } from './components';
 import Home from './container/Home';
-import {GoogleOAuthProvider} from '@react-oauth/google'
-function App() {
+
+const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const User = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+
+    if (!User) navigate('/login');
+  }, []);
+
   return (
-<GoogleOAuthProvider clientId = {`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}>
-<Routes>
-  <Route path='login' element={<Login/>} />
-  <Route path='/*' element={<Home />} />
-</Routes>
-</GoogleOAuthProvider>
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route path="/*" element={<Home />} />
+    </Routes>
   );
-}
+};
+
 export default App;
